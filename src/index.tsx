5f459c8b9df09b1d13a76c3316a0bb27efd57a3b
Dereck BOURGEOIS <ivoiran20142014@gmail.com>
2024-03-20 18:11:56 +0000

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,16 @@ import { App } from './App';
 import { store } from './states/store';
 import { Provider } from 'react-redux';
 import { CanvasProvider } from './states/CanvasContext';
-import { HTML5Backend as Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <DndProvider backend={Backend}>
-      <Provider store={store}>
-        <CanvasProvider>
-          <App />
-        </CanvasProvider>
-      </Provider>
-    </DndProvider>
+    <Provider store={store}>
+      <CanvasProvider>
+        <App />
+      </CanvasProvider>
+    </Provider>
   </React.StrictMode>
 );
